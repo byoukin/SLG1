6e4b3652e136d570d7e8332d35057c4b82440713
byoukin <byoukin@gmail.com>
2015-09-15 14:16:22 -0400

    minor commit

--- src/main/Weapon.java
+++ src/main/Weapon.java
@@ -4,7 +4,9 @@ public class Weapon {
 
 	//Specs of this weapon
 	int dmg, hit;
+	String type;
 	public Weapon(){}
 	public int getDmg(){return dmg;}
 	public int getHit(){return hit;}
+	public String getType() {return type;}
 }
