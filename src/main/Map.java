20f98f3d0c2ea7823477e2d3e214b5ea125e9b7d
byoukin <byoukin@gmail.com>
2015-09-17 16:58:23 -0400

    Add main file and some basic stuff of map.

--- src/main/Map.java
+++ src/main/Map.java
@@ -1,7 +1,22 @@
 package main;
 
+import java.awt.Image;
+import java.awt.Graphics;
+
 public class Map {
-	public Map() {
+	
+	private int[][] loc = null;
+	private numCell;
+	private Image[][] mapImages = null;
+	
+	public Map(int[][] loc, Image[][] mapImages, int numCell){
+		this.loc = loc;
+		this.mapImages = mapImages;
+		this.numCell = numCell;
+	}
+	
+	private void drawMap(Graphics g){
 		
 	}
+		
 }
