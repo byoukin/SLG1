0a773f47b2c583595807532495c797f9850e4686
Yifeng Shi <byoukin@gmail.com>
2015-12-23 21:49:15 +0800

    Ver. 0.0.6
    Changed executible locations.

--- src/main/GameMap.java
+++ src/main/GameMap.java
@@ -7,20 +7,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
 import javax.swing.JFrame;
 
 public class GameMap extends JFrame {
 	
 	private static final long serialVersionUID = 1L;
-	private Toolkit kit = Toolkit.getDefaultToolkit();
+	private String stage = null;
+	private Toolkit tk = Toolkit.getDefaultToolkit();
+	private Image mountain = tk.getImage(this.getClass().getResource("/main/cells/mountain.png"));
+	private Image ground = tk.getImage(this.getClass().getResource("/main/cells/ground.png"));
+	private Image forest = tk.getImage(this.getClass().getResource("/main/cells/forest.png"));
+	private Image river = tk.getImage(this.getClass().getResource("/main/cells/river.png"));
+	private Image castle = tk.getImage(this.getClass().getResource("/main/cells/castle.png"));
+	
 	public GameMap(String fileName) throws IOException{
-	//	readFile(fileName);
+		setSize(2560,1440);
+		setVisible(true);
+		
+		stage = fileName;
+		
+	}
+	
+	public void paint(Graphics g){
+		try {
+			drawMap(getStage(),g);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		repaint();
+	}
+	
+	public void update(Graphics g){
+		paint(g);
+	}
+	
+	public String getStage(){
+		return stage;
 	}
 	
 	public String[] readFile(String fileName) throws IOException {
 		File file = new File(fileName);
-		String mapCell[] = new String[20];
+		String mapCell[] = new String[10];
 		int count = 0;
 		BufferedReader reader = new BufferedReader(new FileReader(file));
 		try{
@@ -28,7 +55,7 @@ public class GameMap extends JFrame {
 			while ((read = reader.readLine()) != null){
 				mapCell[count] = read;
 				count++;
-				System.out.println(read);
+			//	System.out.println(read);
 			}
 			reader.close();
 		}catch (Exception e){
@@ -37,9 +64,22 @@ public class GameMap extends JFrame {
 		return mapCell;
 	}
 	
-	public void drawMap(Graphics g){
-		Image abc = kit.getImage(this.getClass().getResource("src/main/cells/mountain.bmp"));
-		g.drawImage(abc, 250, 250, this);
+	public void drawMap(String maps, Graphics g) throws IOException{
+		String[] map = readFile(maps);
+		int count = 0;
+		if (map != null){
+			for (int i = 0; i < map.length; i++){
+				System.out.print(map[i]==null);
+				for (int j = 0; j < map[i].length(); j++){
+				//	System.out.println(count++);
+					if (map[i].charAt(j) == 'G')
+						g.drawImage(ground, 100+50*j, 100+50*i, this);
+					else if (map[i].charAt(j) == 'M')
+						g.drawImage(mountain, 100+50*j, 100+50*i, this);
+					else if (map[i].charAt(j) == 'R')
+						g.drawImage(river, 100+50*j, 100+50*i, this);
+				}
+			}
+		}	
 	}
-		
 }
--- src/main/Startup.java
+++ src/main/Startup.java
@@ -5,38 +5,19 @@ import java.awt.*;
 import javax.swing.*;
 
 public class Startup extends JFrame{
+	private GameMap map1 = new GameMap ("bin/main/maps/stage1.txt");
 	
 	private static final long serialVersionUID = -52208218690144130L;
-	public Startup() throws IOException{
-		init();
-	}
-	public static void main(String args[]) throws IOException{
-		new Startup();
-	}
-	
-	public void init() throws IOException {
-
-		setSize(2560, 1440);
-		setBackground(Color.black);
-		setVisible(true);
-		
-//		map1 = new GameMap("bin/main/maps/stage1.txt");
+	public Startup() throws IOException{}
+	public static void main(String args[]) throws IOException{	
+		new Startup();	
 	}
 
-//	private GameMap map1;
-	private Toolkit tk = Toolkit.getDefaultToolkit();
-	private Image a1 = tk.getImage(this.getClass().getResource("/main/cells/mountain.png"));
 	public void paint(Graphics g){
-		g.drawImage(a1, 250, 250, this);
 		repaint();
-	}
-	
-	public void Menu(Graphics g){
-		
 	}
 	
 	public void update(Graphics g){
 		paint(g);
 	}
-
 }
